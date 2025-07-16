4ed3fa8cea9f13ad88aa293064b2c7b8fac15028
Dean Gilmore <dean@videoremix.io>
2025-07-16 20:48:15 +0000

    Remove debug logging - app should be working now

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
-import './debug-env';
 import { ErrorBoundary } from './components/ErrorBoundary';
 
 createRoot(document.getElementById('root')!).render(
